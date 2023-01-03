ceecd8c9b07da24c44b2f9978fa2f4d1aeb89642
Durbek <pattoyevd@gmail.com>
2023-01-03 19:52:37 +0500

    fixed bug

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-urlpatterns = (
+urlpatterns = [
     path('admin/', admin.site.urls),
     path("urls/", include("amCharts.urls")),
-)
+]
