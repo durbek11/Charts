5ebc0df0fae5b8189597e1f502b2968fa20f79d8
Durbek <pattoyevd@gmail.com>
2023-01-06 21:12:04 +0500

    add fiels

--- core/settings.py
+++ core/settings.py
@@ -104,8 +104,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATICFILES_DIRS =(
+    BASE_DIR / "static",
+)
+
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staicfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
