97b7c738f3c9bb0bcae608402210725690db6503
Durbek <pattoyevd@gmail.com>
2023-01-22 22:26:51 +0500

    add login

--- amCharts/urls.py
+++ amCharts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, followToggle
 
 app_name = "app"
 
 urlpatterns = [
     path('', home, name="home"),
+    path('login/', followToggle, name="login")
 ]
