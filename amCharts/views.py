bdf7cfd3d91a5285d2dc931830324274cdc2c937
Durbek <pattoyevd@gmail.com>
2023-01-22 22:22:32 +0500

    add login

--- amCharts/views.py
+++ amCharts/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'pages/home.html')
 
+def followToggle(request, author):
+    if request.user.is_authenticated:
+        authorObj = User.objects.get(username=author)
+        currentUserObj = User.objects.get(username=request.user.username)
+        following = authorObj.following.all()
+
+        if author != currentUserObj.username:
+            if currentUserObj in following:
+                authorObj.following.remove(currentUserObj.id)
+            else:
+                authorObj.following.add(currentUserObj.id)
+
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    else:
+        return redirect('app:login')
+
