0af8e4c7ca009bb17949ac211327ec6abccbe657
Durbek <pattoyevd@gmail.com>
2023-01-24 21:15:06 +0500

    add imgs

--- amCharts/urls.py
+++ amCharts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, followToggle
+from .views import home, followToggle, signup
 
 app_name = "app"
 
 urlpatterns = [
     path('', home, name="home"),
+    path("signup/", signup, name="signup"),
     path('login/', followToggle, name="login")
 ]
