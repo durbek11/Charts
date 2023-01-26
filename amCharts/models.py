d68b26dd686472debbc1d8aa98afe4d30364e49f
Durbek <pattoyevd@gmail.com>
2023-01-26 18:23:19 +0500

    add chart model

--- amCharts/models.py
+++ amCharts/models.py
@@ -2,3 +2,20 @@ from audioop import add
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
+class Chart(models.Model):
+    PIE = "Pie"
+    COLUMN = "Column"
+    CHART_CHOICES = [
+        (PIE, "Pie Chart"),
+        (COLUMN, "Column Chart")
+    ]
+    author = models.ForeignKey(MyUser, on_delete=models.CASCADE,related_name='chart' )
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(unique=True)
+    caption = models.TextField(max_length=700)
+    created_on = models.DateTimeField(("date joined"), default=timezone.now)
+    chart_type = models.CharField(max_length=15,choices=CHART_CHOICES, default=PIE)
+    def __str__(self):
+        return self.name
+
