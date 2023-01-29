ca0f8b4ec239cdb30662c6cab03f9239778e1d69
Durbek <pattoyevd@gmail.com>
2023-01-29 21:46:31 +0500

    commit

--- amCharts/models.py
+++ amCharts/models.py
@@ -23,3 +23,19 @@ class ContactUs(models.Model):
     def __str__(self):
         return f"{self.id}, subject: {self.subject}"
 
+class Chart(models.Model):
+    PIE = "Pie"
+    COLUMN = "Column"
+    CHART_CHOICES = [
+        (PIE, "Pie Chart"),
+        (COLUMN, "Column Chart")
+    ]
+    author = models.ForeignKey(MyUser, on_delete=models.CASCADE,related_name='chart' )
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(unique=True)
+    caption = models.TextField(max_length=700)
+    created_on = models.DateTimeField(("date joined"), default=timezone.now)
+    chart_type = models.CharField(max_length=15,choices=CHART_CHOICES, default=PIE)
+    def __str__(self):
+        return self.name
+
