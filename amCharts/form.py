a2ae353d00c431dfe3abc24b40ac2badb0894d8d
Durbek <pattoyevd@gmail.com>
2023-02-02 21:22:14 +0500

    commit

--- amCharts/form.py
+++ amCharts/form.py
@@ -15,3 +15,75 @@ class Registration(UserCreationForm):
         lowercase_username = username.lower()         # get the lowercase version of it
 
         return lowercase_username
+
+class ChartFrom(forms.ModelForm):
+    class Meta:
+        model = Chart
+        fields = ("name", "chart_type","slug", "caption")
+    
+    def clean_slug(self):
+        slug = self.cleaned_data.get("slug")
+        lowercase_slug = slug.lower()
+
+        return lowercase_slug
+
+class ElementForm(forms.ModelForm):
+    class Meta:
+        model = Element
+        fields = ("title", "value")
+
+class UpdateUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', "username", "email"]
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')  # get the username data
+        lowercase_username = username.lower()         # get the lowercase version of it
+
+        return lowercase_username
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["image", "bio"]
+    
+    def clean_avatar(self):
+        avatar = self.cleaned_data['image']
+
+        try:
+            w, h = get_image_dimensions(avatar)
+
+            #validate dimensions
+            max_width = max_height = 100
+            if w == max_width or h == max_height:
+                raise forms.ValidationError(
+                    u'Please use an image that is '
+                     '%s x %s pixels or smaller.' % (max_width, max_height))
+
+            #validate content type
+            main, sub = avatar.content_type.split('/')
+            if not (main == 'image' and sub in ['jpeg', 'pjpeg', 'gif', 'png']):
+                raise forms.ValidationError(u'Please use a JPEG, '
+                    'GIF or PNG image.')
+
+            #validate file size
+            if len(avatar) > (20 * 1024):
+                raise forms.ValidationError(
+                    u'Avatar file size may not exceed 20k.')
+
+        except AttributeError:
+
+            pass
+
+        return avatar
+
+class ContactUsForm(forms.ModelForm):
+    class Meta:
+        model = ContactUs
+        fields = ["name", "email", "subject", "message"]
+
+class AccountVerifyForm(forms.ModelForm):
+    class Meta:
+        model = accountVerify
+        fields = ['email', 'verfy_account1','message',]
